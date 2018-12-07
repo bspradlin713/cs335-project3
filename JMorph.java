87545f39ec328f05935159d681c44f72ca158b05
bspradlin713 <43010306+bspradlin713@users.noreply.github.com>
2018-12-07 17:37:28 -0500

    Fixed the bugs with control points and preview

--- JMorph.java
+++ JMorph.java
@@ -1,14 +1,25 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class JMorph {
     JFrame frame;
-    int animationDuration = 1;
+    JPanel mainPanel;
+    panelHandler handler;
+    int animationDuration = 5;
     int gridResolution = 10;
+    JFileChooser picker; //Maybe add in something to get working directory
+
+    private BufferedImage prePic;
+    private BufferedImage postPic;
 
     public static void main(String args[]){
         new JMorph();
@@ -16,19 +27,21 @@ public class JMorph {
 
     public JMorph(){
         frame = new JFrame("JMorph");
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+        frame.add(mainPanel);
 
         buildMenus();
         buildControls();
-
-        //add grids
+        buildGrids();
 
         frame.pack();
         frame.setVisible(true);
         frame.addWindowListener (new WindowAdapter() {
-                public void windowClosing (WindowEvent e) {
-                    System.exit(0);
-                }
-            }
+                                     public void windowClosing (WindowEvent e) {
+                                         System.exit(0);
+                                     }
+                                 }
         );
     }
 
@@ -41,16 +54,66 @@ public class JMorph {
         JMenu settingsMenu = new JMenu("Settings");
         menuBar.add(settingsMenu);
 
-        JMenuItem openFileMenuItem = new JMenuItem("Open");
-        openFileMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
+        //Stuff with the file chooser
+        picker = new JFileChooser();
+        picker.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        picker.addChoosableFileFilter(new FileNameExtensionFilter("Image Files", "jpg", "png", "tif"));
+        picker.setAcceptAllFileFilterUsed(false);
 
+        JMenuItem openFileMenuItemPre = new JMenuItem("Open Pre Picture");
+        openFileMenuItemPre.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int validFile = picker.showOpenDialog(frame);
+                if (validFile == JFileChooser.APPROVE_OPTION){
+                    try{
+                        prePic = ImageIO.read(picker.getSelectedFile());
+                    } catch(IOException exc){
+                        System.out.println("Problem reading file");
+                    };
+                    handler.setPrePic(prePic);
+                }
             }
         });
-        //fileMenu.add(openFileMenuItem);
+        fileMenu.add(openFileMenuItemPre);
 
-        //fileMenu.addSeparator();
+        JMenuItem openFileMenuItemPost = new JMenuItem("Open Post Picture");
+        openFileMenuItemPost.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int validFile = picker.showOpenDialog(frame);
+                if (validFile == JFileChooser.APPROVE_OPTION){
+                    try{
+                        postPic = ImageIO.read(picker.getSelectedFile());
+                    } catch(IOException exc){
+                        System.out.println("Problem reading file");
+                    };
+                    handler.setPostPic(postPic);
+                }
+            }
+        });
+        fileMenu.add(openFileMenuItemPost);
+        fileMenu.addSeparator();
+
+        JMenuItem boostImage1 = new JMenuItem("Brighten Image 1");
+        boostImage1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handler.boostPreImage();
+            }
+        });
+        fileMenu.add(boostImage1);
+        JMenuItem boostImage2 = new JMenuItem("Brighten Image 2");
+        boostImage2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handler.boostPostImage();
+            }
+        });
+        fileMenu.add(boostImage2);
+        fileMenu.addSeparator();
+
+
         JMenuItem exitMenuItem = new JMenuItem("Exit");
         exitMenuItem.addActionListener(new ActionListener() {
             @Override
@@ -77,6 +140,7 @@ public class JMorph {
                         }
                         else{
                             animationDuration = convertedInput;
+                            handler.setDuration(animationDuration);
                         }
                     }
 
@@ -109,6 +173,7 @@ public class JMorph {
                         }
                         else{
                             gridResolution = convertedInput;
+                            handler.changeResolution(gridResolution, gridResolution);
                         }
                     }
 
@@ -130,7 +195,7 @@ public class JMorph {
     private void buildControls(){
         JPanel controlPanel = new JPanel();
         controlPanel.setLayout(new FlowLayout());
-        frame.add(controlPanel);
+        mainPanel.add(controlPanel);
 
         JButton previewButton = new JButton("Preview");
         previewButton.addActionListener(new ActionListener() {
@@ -145,9 +210,23 @@ public class JMorph {
         previewButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                //call function to morph the two images
+                previewAnimation();
             }
         });
         //controlPanel.add(morphButton);
+    }
+
+    private void buildGrids(){
+        handler = new panelHandler(gridResolution, gridResolution);
+        mainPanel.add(handler);
+    }
+
+    private void previewAnimation(){
+        JFrame previewFrame = new JFrame("JMorph [PREVIEW]");
+
+        handler.animateGrid(previewFrame);
+
+        previewFrame.pack();
+        previewFrame.setVisible(true);
     }
 }
